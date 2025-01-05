59c59a75cf21f25b429f82579505972782a19c4f
sam <sam@gmail.com>
2025-01-05 14:57:18 -0500

    add homebrew check and keyboard repeat rate line

--- macsetup.sh
+++ macsetup.sh
@@ -1,3 +1,13 @@
-# install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# install homebrew
+if ! command -v brew &>/dev/null; then
+  echo "installing brew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+brew install zsh
+
+# set typing speed to be quick
+defaults write NSGlobalDomain InitialKeyRepeat -int 20
+defaults write NSGlobalDomain KeyRepeat -int 1
 
